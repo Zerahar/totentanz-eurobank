e99d7ea859ada13cdf92a9a0c511694665a55803
Zerahar <heinisaastamoinen@gmail.com>
2026-02-26 09:02:50 +0200

    Init

---
+++ client/script.js
@@ -0,0 +1,483 @@
+$(function () {
+
+    const serverUrl = "http://localhost:3000/";
+    var currentUser;
+    var userList;
+    var lastHacked;
+    var hackCooldown;
+    var isAdmin = false;
+    var isHacker = false;
+    var updateInterval;
+
+    const $list = $("#PlayerList");
+    const $paymentBox = $("#PaymentBox");
+    const $hackBox = $("#HackBox");
+    const $deleteBox = $("#DeleteBox");
+    const $errorView = $("#ErrorView");
+    const $messageView = $("#MessageView");
+
+    $(".close-btn").on('click', closeBox);
+    $("#PayButton").on("click", pay);
+    $("#HackButton").on("click", hack);
+    $("#LoginButton").on("click", login);
+    $("#LogoutButton").on("click", logout);
+    $("#DeleteButton").on("click", deleteUser);
+    $("#ShowAddUser").on("click", showAddUser);
+    $('#AddUserBtn').on('click', addUser);
+    $('#EditUserBtn').on('click', editUser);
+
+    async function login() {
+        clearError();
+        var password = $("#Password").val();
+        $("#Loader").show();
+        try {
+            const response = await fetch(serverUrl + "login/" + password);
+            if (!response.ok) {
+                console.log(`Response status: ${response.status}`);
+                showError("Kirjautuminen epäonnistui");
+                return;
+            }
+
+            const json = await response.json();
+            currentUser = json.currentUser;
+            $("#LoginContainer").hide();
+            $("#LogoutButton").show();
+
+            $("#CurrentCredit").html(json.currentCredits);
+            $("#LoggedName").text(json.currentUser);
+            $("#LoggedView").show();
+
+            userList = json.players;
+            lastHacked = json.lastHacked;
+            hackCooldown = json.hackCooldown;
+
+            if (updateInterval) clearInterval(updateInterval);
+            if (json.type == "admin") {
+                isAdmin = true;
+                $("#CurrentCreditRow").hide();
+                $("#AddUserContainer,#ShowAddUser").show();
+                updateInterval = setInterval(() => {
+                    getUsers()
+                }, 15000)
+            } else {
+                isAdmin = false;
+                $("#CurrentCreditRow").show();
+                $("#AddUserContainer").hide();
+                updateInterval = setInterval(() => {
+                    getUpdate()
+                }, 15000)
+            }
+            isHacker = !isAdmin && json.is_hacker == 1;
+
+            generatePlayerList(json.currentCredits);
+
+            if (lastHacked) {
+                $('#HackWarning').show();
+                if (json.is_corp == 1) {
+                    $('#HackSource').show();
+                    $('#HackerName').text(json.last_hacker);
+                }
+            }
+
+            $("#Loader").hide();
+
+        } catch (error) {
+            console.error(error.message);
+            $("#Loader").hide();
+            showError("Kirjautuminen epäonnistui");
+        }
+    }
+
+    async function getUsers() {
+        try {
+            const response = await fetch(serverUrl + "users/" + currentUser);
+            if (!response.ok) {
+                console.log(`Response status: ${response.status}`);
+                showError("Käyttäjien haku epäonnistui");
+                return;
+            }
+            const json = await response.json();
+            userList = json;
+            generatePlayerList($("#CurrentCredit").val());
+        } catch (error) {
+            console.error(error.message);
+            showError("Käyttäjien haku epäonnistui");
+        }
+    }
+
+    function logout() {
+        clearError();
+        $("#LoginContainer, #CurrentCreditRow").show();
+        $("#LogoutButton,#LoggedView,#AddUserContainer,#AddUserInputContainer,#HackWarning,#HackSource").hide();
+        $("#CurrentCredit,#HackerName,#LoggedName").text("");
+        $list.empty();
+        $paymentBox.hide();
+        currentUser = null;
+        if (updateInterval) clearInterval(updateInterval);
+    }
+
+    async function getUpdate() {
+        const response = await fetch(serverUrl + "status/" + currentUser);
+        if (!response.ok) {
+            console.log(`Automatic update fail: ${response.message}`);
+            showError("Käyttäjien haku epäonnistui");
+            return;
+        }
+        const json = await response.json();
+        userList = json.users;
+        const hackCooldown = json.hack_cooldown;
+        var d = new Date();
+        d.setMinutes(d.getMinutes() - 1);
+        if (hackCooldown != null && new Date(Date.parse(hackCooldown)) > d) {
+            $('.hack-btn').prop('disabled', true);
+        } else {
+            $('.hack-btn').prop('disabled', false);
+        }
+        const credits = json.credits;
+        if (credits < 1) {
+            $('.pay-btn').prop('disabled', true);
+        } else {
+            $('.pay-btn').prop('disabled', false);
+        }
+    }
+
+    function generatePlayerList(credits) {
+        $list.empty();
+        userList.forEach(player => {
+            if (player.is_admin == 1) return;
+
+            const $cont = $("<div>").addClass("player-node").text(player.name);
+
+            if (isAdmin) {
+                credits = 1;
+                const $credits = $("<span>")
+                    .addClass("credits")
+                    .text("(Rahaa: " + player.credits + ')');
+                $cont.append($credits);
+            }
+
+            const $btnContainer = $('<div class="btn-container">');
+            $cont.append($btnContainer);
+            const $payBtn = $("<button>")
+                .addClass("pay-btn")
+                .text("Maksa")
+                .attr('data-name', player.name)
+                .attr('data-action', 'pay')
+                .on("click", openBox);
+            if (credits < 1) {
+                $payBtn.prop('disabled', true);
+            }
+            $btnContainer.append($payBtn);
+
+            if (isAdmin) {
+                const $editBtn = $("<button>")
+                    .addClass("edit-btn")
+                    .text("Muokkaa")
+                    .attr('data-name', player.name)
+                    .attr('data-action', 'edit')
+                    .on("click", showEditUser);
+                $btnContainer.append($editBtn);
+                const $resetBtn = $("<button>")
+                    .addClass("reset-btn")
+                    .text("Nollaa")
+                    .attr('data-name', player.name)
+                    .attr('data-action', 'reset')
+                    .on("click", resetUser);
+                $btnContainer.append($resetBtn);
+                const $deleteBtn = $("<button>")
+                    .addClass("delete-btn")
+                    .text("Poista")
+                    .attr('data-name', player.name)
+                    .attr('data-action', 'delete')
+                    .on("click", openBox);
+                $btnContainer.append($deleteBtn);
+                if (player.hack_cooldown) $cont.append(`<span>Viimeksi hakkeroinut ${new Date(player.hack_cooldown).toLocaleString()}</span>`);
+                if (player.last_hacked) $cont.append(`<span>Viimeksi jäänyt hakkerin ${player.last_hacker} kohteeksi ${new Date(player.last_hacked).toLocaleString()}</span>`);
+            }
+
+            if (isHacker) {
+                const $hackBtn = $("<button>")
+                    .addClass("hack-btn")
+                    .text("Hakkeroi")
+                    .attr('data-name', player.name)
+                    .attr('data-action', 'hack')
+                    .on("click", openBox);
+                var d = new Date();
+                d.setMinutes(d.getMinutes() - 1);
+                if (hackCooldown != null && new Date(Date.parse(hackCooldown)) > d) {
+                    $hackBtn.prop('disabled', true);
+                }
+                $btnContainer.append($hackBtn);
+            }
+            $list.append($cont);
+        });
+    }
+
+    function openBox(e) {
+        const target = $(e.target).attr("data-name");
+        const action = $(e.target).attr("data-action");
+        closeEdit();
+
+        var $box;
+        if (action == "pay") {
+            $box = $paymentBox;
+            $("#PaymentTarget").html(target);
+        } else if (action == "hack") {
+            $box = $hackBox;
+            $("#HackTarget").html(target);
+            $('#HackBtn').prop('disabled', false)
+        } else if (action == "delete") {
+            $box = $deleteBox;
+            $("#DeleteTarget").html(target)
+        }
+
+        $box.show();
+    }
+
+    function closeEdit() {
+        getUsers();
+        $("#ShowAddUser, #AddUserBtn").show();
+        $("#AddUserInputContainer, #EditUserBtn").hide();
+        $('#EditUserBtn').attr('data-name', "");
+    }
+
+    function closeBox() {
+        $paymentBox.hide();
+        $hackBox.hide();
+        $deleteBox.hide();
+        clearError();
+    }
+
+    async function pay() {
+        clearError();
+        const user = $("#PaymentTarget").text();
+        const amount = $("#PaymentAmount").val();
+        const current = $("#CurrentCredit").text();
+
+        if (isNaN(parseInt(amount)) || isNaN(parseInt(current))) {
+            console.log("Payment error");
+            $("#PaymentError").html("Virhe maksun määrässä");
+        }
+        if (!isAdmin && parseInt(amount) > parseInt(current)) {
+            console.log("Payment error");
+            $("#PaymentError").html("Ei tarpeeksi rahaa");
+            return;
+        }
+        if (!isAdmin && amount < 1) {
+            console.log("Payment error");
+            $("#PaymentError").html("Summa ei voi olla negatiivinen");
+            return;
+        }
+
+        try {
+            $('#Loader').show();
+            const response = await fetch(serverUrl + "pay/" + user + '/' + amount + '/' + currentUser);
+            if (!response.ok) {
+                console.log(`Response status: ${response.status}`);
+                $("#PaymentError").html("Maksu epäonnistui");
+            }
+
+            const json = await response.json();
+            var currentdata = json.find(x => x.name == currentUser);
+            var targetUser = json.find(x => x.name != currentUser);
+            var targetIndex = userList.findIndex(x => x.name == targetUser.name);
+            userList[targetIndex].credits = targetUser.credits;
+            if (!isAdmin) {
+                $("#CurrentCredit").text(currentdata.credits);
+                $("#PaymentMessage").html("Maksu suoritettu, saldo: " + currentdata.credits);
+                generatePlayerList(currentdata.credits);
+            } else {
+                $("#PaymentMessage").html("Maksu suoritettu");
+                generatePlayerList(1);
+            }
+            console.log(json);
+            $('#Loader').hide();
+        } catch (error) {
+            console.error(error.message);
+            $('#Loader').hide();
+        }
+    }
+
+    function hack() {
+        $("#Loader").show();
+        setTimeout(async function () {
+            try {
+                const target = $('#HackTarget').text();
+                const response = await fetch(serverUrl + "hack/" + target + '/' + currentUser);
+                const json = await response.json();
+                $("#Loader").hide();
+                $("#HackSuccess").toggle(json.status);
+                $("#HackFailure").toggle(!json.status);
+                $('#HackBtn').prop('disabled', true);
+                $('.hack-btn').prop('disabled', true);
+                if (!isNaN(json.amount)) $('#HackAmount').text(json.amount);
+            } catch (e) {
+                console.log(e);
+                $("#Loader").hide();
+                $("#HackSuccess").hide();
+                $("#HackFailure").show();
+            }
+            // Update hack cooldown
+            $('.hack-btn').prop('disabled', true);
+        }, 3000);
+    }
+
+    function showAddUser() {
+        $("#ShowAddUser").hide();
+        $("#AddUserInputContainer").show();
+        $("#NewUserName").val("");
+        $("#NewUserPassword").val("");
+        $('#NewUserCredits').val("");
+        $('#NewUserHackChance').val("");
+        $('#NewUserIsHacker').prop('checked', false);
+        $('#NewUserIsCorp').prop('checked', false);
+    }
+
+    async function addUser() {
+        clearError();
+        // Validate
+        var valid = $('#AddUserInputContainer').valid();
+        if (!valid) showError("Tarkista kentät!");
+
+        const username = $("#NewUserName").val();
+        const password = $("#NewUserPassword").val();
+        const credits = $('#NewUserCredits').val();
+        const hack_chance = $('#NewUserHackChance').val();
+        const is_hacker = $('#NewUserIsHacker').is(':checked');
+        const is_corp = $('#NewUserIsCorp').is(':checked');
+        try {
+            $("#Loader").show();
+            const response = await fetch(serverUrl + "new", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username,
+                    password,
+                    credits,
+                    hack_chance,
+                    is_hacker,
+                    is_corp
+                })
+            });
+            if (!response.ok) showError(response.status == 409 ? "Samanniminen käyttäjä on jo olemassa" : "Käyttäjän lisääminen epäonnistui");
+            else {
+                showMessage("Käyttäjän lisääminen onnistui")
+                closeEdit();
+            }
+            $("#Loader").hide();
+        } catch (error) {
+            showError("Käyttäjän lisääminen epäonnistui");
+            console.error(error.message);
+            $("#Loader").hide();
+        }
+    }
+
+    function showEditUser(e) {
+        clearError();
+        const username = $(e.target).attr('data-name');
+        const user = userList.find(x => x.name == username);
+        $("#NewUserName").val(username);
+        $("#NewUserPassword").val(user.password);
+        $('#NewUserCredits').val(user.credits);
+        $('#NewUserHackChance').val(user.hack_chance);
+        $('#NewUserIsHacker').prop('checked', user.is_hacker);
+        $('#NewUserIsCorp').prop('checked', user.is_corp);
+        $("#ShowAddUser, #AddUserBtn").hide();
+        $("#AddUserInputContainer, #EditUserBtn").show();
+        $('#EditUserBtn').attr('data-name', username);
+    }
+
+    async function editUser() {
+        clearError();
+        // Validate
+        var valid = $('#AddUserInputContainer').valid();
+        if (!valid) showError("Tarkista kentät!");
+
+        const username = $("#NewUserName").val();
+        const password = $("#NewUserPassword").val();
+        const credits = $('#NewUserCredits').val();
+        const hack_chance = $('#NewUserHackChance').val();
+        const is_hacker = $('#NewUserIsHacker').is(':checked');
+        const is_corp = $('#NewUserIsCorp').is(':checked');
+        const old_name = $('#EditUserBtn').attr('data-name');
+        try {
+            $("#Loader").show();
+            const response = await fetch(serverUrl + "edit", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username,
+                    password,
+                    credits,
+                    hack_chance,
+                    is_hacker,
+                    is_corp,
+                    old_name
+                })
+            });
+            if (!response.ok) showError("Käyttäjän tallentaminen epäonnistui");
+            else {
+                showMessage("Käyttäjän tallentaminen onnistui");
+                closeEdit();
+            }
+            $("#Loader").hide();
+        } catch (error) {
+            showError("Käyttäjän tallentaminen epäonnistui");
+            console.error(error.message);
+            $("#Loader").hide();
+        }
+    }
+
+    async function deleteUser() {
+        clearError();
+        $("#Loader").show();
+        const target = $('#DeleteTarget').text();
+        try {
+            const response = await fetch(serverUrl + "delete/" + target);
+            if (!response.ok) showError("Käyttäjän poisto epäonnistui");
+            $("#Loader").hide();
+            closeBox();
+        } catch (error) {
+            showError("Käyttäjän poisto epäonnistui");
+            console.error(error.message);
+            $("#Loader").hide();
+            closeBox();
+        }
+        getUsers();
+    }
+
+    async function resetUser(e) {
+        clearError();
+        $("#Loader").show();
+        const target = $(e.target).attr('data-name');
+        try {
+            const response = await fetch(serverUrl + "reset/" + target);
+            if (!response.ok) showError("Käyttäjän nollaus epäonnistui");
+            else showMessage("Käyttäjän nollaus onnistui")
+            $("#Loader").hide();
+        } catch (error) {
+            showError("Käyttäjän nollaus epäonnistui");
+            console.error(error.message);
+            $("#Loader").hide();
+        }
+    }
+
+    function showError(error) {
+        $errorView.html(error);
+    }
+
+    function showMessage(msg) {
+        $messageView.html(msg);
+    }
+
+    function clearError() {
+        $errorView.html("");
+        $messageView.html("");
+        $("#PaymentError, #PaymentMessage,#HackAmount").html("");
+        $('#HackFailure, #HackSuccess').hide();
+    }
+});
---
+++ server/app.js
@@ -0,0 +1,265 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'totentanz'
+});
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const port = 3000
+const app = express()
+
+// CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+// create application/json parser
+const jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded()
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server running on port 3000');
+});
+
+// Create user
+app.post('/new', jsonParser, function (req, res) {
+    connection.query(`INSERT INTO users (name, password, credits, hack_chance, is_hacker, is_corp) VALUES ('${req.body.username}', '${req.body.password}', '${req.body.credits}', '${req.body.hack_chance}', '${req.body.is_hacker == true ? 1 : 0}', '${req.body.is_corp == true ? 1 : 0}')`, (err, rows) => {
+        if (err) {
+            res.status(err.errno == 1062 ? 409 : 500);
+            res.send(err.sqlMessage);
+            return;
+        }
+        res.send(true);
+    });
+})
+
+// Edit user
+app.post('/edit', jsonParser, function (req, res) {
+    connection.query(`UPDATE users SET name = '${req.body.username}', password = '${req.body.password}', credits = '${req.body.credits}', hack_chance = '${req.body.hack_chance}', is_hacker = '${req.body.is_hacker == true ? 1 : 0}', is_corp ='${req.body.is_corp == true ? 1 : 0}' WHERE name = '${req.body.old_name}'`, (err, rows) => {
+        if (err) {
+            res.status(500)
+            res.send(err.sqlMessage);
+            return;
+        }
+        res.send(true);
+    });
+})
+
+// Pay to user
+app.get('/pay/:user/:amount/:from', (req, res) => {
+    connection.query(`UPDATE users SET credits = credits +${req.params.amount} WHERE NAME = '${req.params.user}'`, (err, rows) => {
+        if (err) {
+            res.status(500);
+            res.send(err.sqlMessage);
+            return;
+        }
+        if (req.params.from != "admin") {
+            connection.query(`UPDATE users SET credits = credits -${req.params.amount} WHERE NAME = '${req.params.from}'`, (err, rows) => {
+                if (err) {
+                    res.status(500);
+                    res.send(err.sqlMessage);
+                    return;
+                }
+                connection.query(`SELECT name, credits FROM users WHERE NAME = '${req.params.user}' OR NAME = '${req.params.from}'`, (err, rows) => {
+                    if (err) {
+                        res.status(500);
+                        res.send(err.sqlMessage);
+                        return;
+                    }
+                    res.send(rows);
+                });
+            });
+        } else {
+            connection.query(`SELECT name, credits FROM users WHERE NAME = '${req.params.user}'`, (err, rows) => {
+                if (err) {
+                    res.status(500);
+                    res.send(err.sqlMessage);
+                    return;
+                }
+                res.send(rows);
+            });
+        }
+    });
+})
+
+// Hack user
+app.get('/hack/:target/:hacker', (req, res) => {
+    // Get chance
+    connection.query(`SELECT hack_chance, credits FROM users WHERE NAME = '${req.params.target}'`, (err, rows) => {
+        if (err) {
+            res.status(500);
+            res.send(err.sqlMessage);
+            return;
+        }
+        // Roll the die
+        const rand = Math.random();
+        const hack_chance = rows[0].hack_chance;
+        const success = rand <= (hack_chance / 100);
+        var stolenAmount = 0;
+        console.log(rand, hack_chance, success);
+        if (success) {
+            // Steal 30% of money
+            stolenAmount = Math.floor(rows[0].credits * 0.3);
+            console.log("Stolen amount: " + stolenAmount)
+            connection.query(`UPDATE users SET credits = (credits - ${stolenAmount}) WHERE NAME = '${req.params.target}'`);
+            connection.query(`UPDATE users SET credits = (credits + ${stolenAmount}) WHERE NAME = '${req.params.hacker}'`);
+            if (err) {
+                res.status(500);
+                res.send(err.sqlMessage);
+                return;
+            }
+        }
+        // Activate cooldown
+        connection.query(`UPDATE users SET hack_cooldown = NOW() WHERE NAME = '${req.params.hacker}'`);
+        if (err) {
+            res.status(500);
+            res.send(err.sqlMessage);
+            return;
+        }
+
+        // Change latest hacked timestamp
+        connection.query(`UPDATE users SET last_hacked = NOW() WHERE NAME = '${req.params.target}'`);
+        if (err) {
+            res.status(500);
+            res.send(err.sqlMessage);
+            return;
+        }
+
+        // Save hacker name
+        if (!success) {
+            connection.query(`UPDATE users SET last_hacker = '${req.params.hacker}' WHERE NAME = '${req.params.target}'`);
+            if (err) {
+                res.status(500);
+                res.send(err.sqlMessage);
+                return;
+            }
+        }
+
+        res.send({ "status": success, "amount": stolenAmount });
+    });
+
+})
+
+// User login
+app.get('/login/:password', (req, res) => {
+    connection.query('SELECT * FROM users', (err, rows) => {
+        if (err) {
+            res.status(500);
+            res.send(err.sqlMessage);
+            return;
+        }
+
+        console.log('Data received from Db:');
+        console.log(rows);
+
+        var loggedIndex = rows.findIndex(x => x.password == req.params.password);
+        if (loggedIndex == -1) {
+            res.status(401);
+            res.send();
+            return;
+        }
+        var loggedUser = rows.splice(loggedIndex, 1);
+        var type = loggedUser[0].is_admin == 1 ? "admin" : "user";
+        var response = {
+            "type": type,
+            "is_hacker": loggedUser[0].is_hacker,
+            "is_corp": loggedUser[0].is_corp,
+            "players": rows,
+            "currentCredits": loggedUser[0].credits,
+            "currentUser": loggedUser[0].name,
+            "lastHacked": loggedUser[0].last_hacked,
+            "last_hacker": loggedUser[0].last_hacker,
+            "hackCooldown": loggedUser[0].hack_cooldown
+        };
+        res.send(response);
+    });
+
+})
+
+// User delete
+app.get('/delete/:username', (req, res) => {
+    connection.query(`DELETE FROM users WHERE name = '${req.params.username}'`, (err, rows) => {
+        if (err) {
+            res.status(500);
+            res.send(err.sqlMessage);
+            return;
+        }
+
+        console.log('Data received from Db:');
+        console.log(rows);
+        res.send("ok");
+    });
+
+})
+
+// User reset
+app.get('/reset/:username', (req, res) => {
+    connection.query(`UPDATE users SET hack_cooldown = NULL, last_hacked = NULL, last_hacker = NULL WHERE name = '${req.params.username}'`, (err, rows) => {
+        if (err) {
+            res.status(500);
+            res.send(err.sqlMessage);
+            return;
+        }
+        res.send("ok");
+    });
+
+})
+// User list
+app.get('/users/:username', (req, res) => {
+    connection.query(`SELECT * FROM users WHERE is_admin = 0 AND name != '${req.params.username}'`, (err, rows) => {
+        if (err) {
+            res.status(500);
+            res.send(err.sqlMessage);
+            return;
+        }
+
+        console.log('Data received from Db:');
+        console.log(rows);
+        var response = rows;
+        res.send(response);
+    });
+
+})
+// Auto update
+app.get('/status/:username', (req, res) => {
+    connection.query(`SELECT * FROM users WHERE is_admin = 0`, (err, rows) => {
+        if (err) {
+            res.status(500);
+            res.send(err.sqlMessage);
+            return;
+        }
+
+        //console.log('Data received from Db:');
+        //console.log(rows);
+        var response = {};
+        const userIndex = rows.findIndex(x => x.name == req.params.username);
+        if (userIndex == -1) {
+            res.status(500);
+            res.send("User not found");
+            return;
+        }
+        const user = rows.splice(userIndex, 1);
+        response.credits = user[0].credits;
+        response.hack_cooldown = user[0].hack_cooldown;
+        response.users = rows;
+        res.send(response);
+    });
+
+})
